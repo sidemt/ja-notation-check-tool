9a46d6722c8b17e1c188078801be8859e4a32877
sidemt <25644062+sidemt@users.noreply.github.com>
2023-03-17 15:24:28 +0900

    Improve layout, add a few more rules

--- index.js
+++ index.js
@@ -20,9 +20,13 @@ const enabled_rules = {
     regex: /[!?]/g,
     hint: "感嘆符、疑問符は、日本語の文章につける場合は全角で入力してください。",
   },
+  halfwidth_punctuation: {
+    regex: /[｡､｢｣･]/g,
+    hint: "半角が使われています。句読点、カギ括弧、中黒は全角で入力してください。",
+  },
   fullwidth_space: {
     regex: /　/g,
-    hint: "全角スペースが使用されています。スペースは半角で入力してください。",
+    hint: "全角スペースが使われています。スペースは半角で入力してください。",
   },
   space_needed: {
     regex: /[\u3041-\u3096\u30A1-\u30FA々〇〻\u3400-\u9FFF\uF900-\uFAFF\uD840-\uD87F\uDC00-\uDFFF][a-zA-Z0-9!#-&*-/<-@\\^-~]|[a-zA-Z0-9!#-&*-/<-@\\^-~][\u3041-\u3096\u30A1-\u30FA々〇〻\u3400-\u9FFF\uF900-\uFAFF\uD840-\uD87F\uDC00-\uDFFF]/g,
@@ -38,7 +42,7 @@ const enabled_rules = {
   },
   space_not_needed: {
     regex: /[\x01-\x7E] [「」！？、。～％・／]|[「」！？、。～％・／] [\x01-\x7E]/g,
-    hint: "全角の記号 (句読点、カギ括弧、％、～など) と半角英数字が隣接する場合、半角スペースを入れないでください。"
+    hint: "全角の記号 (句読点、カギ括弧、％、～など) と半角英数字の間には、スペースは入れないでください。"
   },
   space_not_needed_ja: {
     regex: /[^\x01-\x7E] [「」！？、。～％・／]|[「」！？、。～％・／] [^\x01-\x7E]|[^\x01-\x7E] [^\x01-\x7E]/g,
@@ -47,7 +51,11 @@ const enabled_rules = {
   multiple_spaces: {
     regex: / {2,}/g,
     hint: "半角スペースが連続しています。必要なスペースかどうか確認してください。"
-  }
+  },
+  wrong_prolonged_mark: {
+    regex: /[－—―]/g,
+    hint: "長音記号とよく似た別の記号が使われています。意図した記号かどうか確認してください。"
+  },
 }
 
 function checkText() {
@@ -64,26 +72,30 @@ function checkText() {
     suggestions = suggestions.concat(matches)
   }
 
-  console.log(suggestions);
-
   // 検出された suggestions を index 順に並び替え
   suggestions.sort((a, b) => {
     return a.index - b.index;
   })
 
-  console.log(suggestions);
-
   let output = "";
+  const notice = document.getElementById("output-notice");
 
-  // 後ろから処理
-  for (let i = suggestions.length - 1; i >= 0; i--) {
-    let suggestion = suggestions[i];
+  if (suggestions.length === 0) {
+    notice.classList.add("visible");
+  } else {
+    notice.classList.remove("visible");
+    // テキストの後ろから処理、ハイライト用の要素を追加していく
+    for (let i = suggestions.length - 1; i >= 0; i--) {
+      let suggestion = suggestions[i];
 
-    // 検出された index までの文字を切り出す
-    let target = stringToCheck.slice(suggestion.index);
-    stringToCheck = stringToCheck.slice(0, suggestion.index);
-    let result = target.replace(suggestion[0], `[[[spanstart]]]${suggestion.rule}[[[spanend]]]$&[[[spanclose]]]`);
-    output = result + output;
+      // 検出された index から末尾までの文字を処理対象として切り出す
+      let target = stringToCheck.slice(suggestion.index);
+      // 先頭から検出された index から末尾までの文字を未処理として残す
+      stringToCheck = stringToCheck.slice(0, suggestion.index);
+      // ユーザーが入力した HTML タグと区別して扱うために、仮タグを入力
+      let result = target.replace(suggestion[0], `[[[spanstart]]]${suggestion.rule}[[[spanend]]]$&[[[spanclose]]]`);
+      output = result + output;
+    }
   }
 
   // 残りの部分を連結する
